e9ee25564cb7d78de4d0574d31b89359cce96d84
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2024-08-14 08:18:57 +0200

    new main window: WeatherWindow

---
+++ WeatherApp/View/WeatherWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace WeatherApp.View;
+
+/// <summary>
+/// Interaction logic for WeatherWindow.xaml
+/// </summary>
+public partial class WeatherWindow : Window
+{
+    public WeatherWindow()
+    {
+        InitializeComponent();
+    }
+}
