3cb10408c80f573a2732b76a39dd1949b37ed42b
Eduardo ARROYO RAMIREZ <eduardo.arroyo@berger-levrault.com>
2024-08-15 09:57:38 +0200

    DatabaseHelper class

---
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -0,0 +1,61 @@
+﻿using SQLite;
+using System.IO;
+
+namespace EvernoteClone.ViewModel.Helpers;
+
+public class DatabaseHelper
+{
+    private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
+
+    public static bool Insert<T>(T item) where T : class
+    {
+        using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+        {
+            conn.CreateTable<T>();
+            if (conn.Insert(item) > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool Update<T>(T item) where T : class
+    {
+        using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+        {
+            conn.CreateTable<T>();
+            if (conn.Update(item) > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static bool Delete<T>(T item) where T : class
+    {
+        using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+        {
+            conn.CreateTable<T>();
+            if (conn.Delete(item) > 0)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static List<T> Read<T>() where T : new()
+    {
+        using (SQLiteConnection conn = new SQLiteConnection(dbFile))
+        {
+            conn.CreateTable<T>();
+            return conn.Table<T>().ToList();
+        }
+    }
+
+}
