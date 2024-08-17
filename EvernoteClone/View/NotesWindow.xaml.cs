5c3b5897374faa81e6c0f2f23b72f4f3e0ceac64
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2024-08-17 13:38:16 +0200

    FontSize and FontFamily controls

--- EvernoteClone/View/NotesWindow.xaml.cs
+++ EvernoteClone/View/NotesWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Controls.Primitives;
 using System.Windows.Documents;
+using System.Windows.Media;
 
 namespace EvernoteClone.View;
 
@@ -12,16 +13,16 @@ public partial class NotesWindow : Window
     public NotesWindow()
     {
         InitializeComponent();
+
+        IOrderedEnumerable<FontFamily> fontFamilies = Fonts.SystemFontFamilies.OrderBy(f => f.Source);
+        fontFamilyCombo.ItemsSource = fontFamilies;
+        List<double> fontSizes = new List<double> { 8, 9, 10 , 11, 12, 14, 16, 28, 48, 72 };
+        fontSizeCombo.ItemsSource = fontSizes;
     }
 
     private void ExitMenuItem_Click(object sender, RoutedEventArgs e)
     {
         Application.Current.Shutdown();
-    }
-
-    private void SpeechButton_Click(object sender, RoutedEventArgs e)
-    {
-
     }
 
     private void contentRichTextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
@@ -42,6 +43,12 @@ public partial class NotesWindow : Window
 
         var selectedDecoration = selection.GetPropertyValue(Inline.TextDecorationsProperty);
         underlineButton.IsChecked = selectedDecoration.Equals(TextDecorations.Underline);
+
+        var selectedFontFamily = selection.GetPropertyValue(Inline.FontFamilyProperty);
+        fontFamilyCombo.SelectedValue = selectedFontFamily;
+
+        var selectedFontSize = selection.GetPropertyValue(Inline.FontSizeProperty);
+        fontSizeCombo.Text = selectedFontSize.ToString();
     }
 
     private void boldButton_Click(object sender, RoutedEventArgs e)
@@ -83,6 +90,22 @@ public partial class NotesWindow : Window
             (contentRichTextBox.Selection.GetPropertyValue(Inline.TextDecorationsProperty) as TextDecorationCollection)
                 .TryRemove(TextDecorations.Underline, out textDecorations);
             contentRichTextBox.Selection.ApplyPropertyValue(Inline.TextDecorationsProperty, textDecorations);
+        }
+    }
+
+    private void fontFamilyCombo_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+    {
+        if (fontFamilyCombo.SelectedItem != null)
+        {
+            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontFamilyProperty, fontFamilyCombo.SelectedValue);
+        }
+    }
+
+    private void fontSizeCombo_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+        if (fontFamilyCombo.SelectedItem != null)
+        {
+            contentRichTextBox.Selection.ApplyPropertyValue(Inline.FontSizeProperty, fontSizeCombo.Text);
         }
     }
 }
