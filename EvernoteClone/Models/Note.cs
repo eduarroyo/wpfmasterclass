d3716a0776e5ba3a4a880d402a073c04540cb0c6
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2024-08-17 09:44:44 +0200

    New project, dependencies and models

---
+++ EvernoteClone/Models/Note.cs
@@ -0,0 +1,11 @@
+﻿namespace EvernoteClone.Models;
+
+public class Note
+{
+    public int Id { get; set; }
+    public int NotebookId { get; set; }
+    public string Title { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public string FileLocation { get; set; }
+}
---
+++ EvernoteClone/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace EvernoteClone.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Lastname { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+}
