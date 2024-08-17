98c2b5485ed7157074f01a46a722a37ecbec8da0
Eduardo Arroyo Ramírez <eduarroyo@gmail.com>
2024-08-17 09:58:31 +0200

    Rename folders and namespaces.

--- EvernoteClone/Model/Note.cs
+++ EvernoteClone/Model/Note.cs
@@ -1,11 +1,21 @@
-﻿namespace EvernoteClone.Models;
+﻿using SQLite;
+
+namespace EvernoteClone.Model;
 
 public class Note
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+
+    [Indexed]
     public int NotebookId { get; set; }
+    
+    [MaxLength(500)]
     public string Title { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public DateTime UpdatedAt { get; set; }
+
     public string FileLocation { get; set; }
 }
--- EvernoteClone/Model/Notebook.cs
+++ EvernoteClone/Model/Notebook.cs
@@ -1,8 +1,15 @@
-﻿namespace EvernoteClone.Models;
+﻿using SQLite;
+
+namespace EvernoteClone.Model;
 
 public class Notebook
 {
+    [PrimaryKey, AutoIncrement]
     public int Id { get; set; }
+
+    [Indexed]
     public int UserId { get; set; }
+
+    [MaxLength(500)]
     public string Name { get; set; }
 }
